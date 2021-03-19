f0417a973d985d77066bd84e10c4e6845688bc2f
thamarais16 <thamarais16@gmail.com>
2021-03-19 17:45:30 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,29 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  templateUrl: "./app.component.html",
+  styleUrls: [ "./app.component.css" ]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  header: string = "header";
+  heading: string = "Welcome";
+  movies: Movie[] =[
+    {title:'Zootopia',director:'Byron Howard, Rich Moore',cast:'Idris Elba, Ginnifer Goodwin, Jason Bateman',releaseDate:'March 4, 2016'},
+    {title:'Batman v Superman: Dawn of Justice',director:'Zack Snyder',cast:'Ben Affleck, Henry Cavill, Amy Adams',releaseDate:'March 25, 2016'},
+    {title:'Captain American: Civil War',director:'Anthony Russo, Joe Russo',cast:'Scarlett Johansson, Elizabeth Olsen, Chris Evans',releaseDate:'May 6, 2016'},
+    {title:'X-Men: Apocalypse',director:'Bryan Singer',cast:'Jennifer Lawrence, Olivia Munn, Oscar Isaac',releaseDate:'May 27, 2016'},
+    {title:'Warcraft',director:'Duncan Jones',cast:'Travis Fimmel, Robert Kazinsky, Ben Foster',releaseDate:'June 10, 2016'},
+  ]
+
+
 }
+
+
+class Movie {
+  title : string;
+  director : string;
+  cast : string;
+  releaseDate : string;
+}
+
