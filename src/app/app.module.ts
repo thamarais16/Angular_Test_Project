d5f311fa375b3581569488f2cccb1550c79cd743
thamarais16 <thamarais16@gmail.com>
2021-04-09 19:42:36 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DatePipe, CurrencyPipe, SlicePipe, UpperCasePipe, LowerCasePipe, TitleC
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 import { TempConverterPipe } from './temp-converter.pipe';
+import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
   imports:      [
@@ -31,7 +32,8 @@ import { TempConverterPipe } from './temp-converter.pipe';
     PipeComponent,
     ChildComponent,
     ParentComponent,
-    TempConverterPipe
+    TempConverterPipe,
+    DirectivesComponent
   ],
   bootstrap:    [ OfficialComponent ],
   providers: [
