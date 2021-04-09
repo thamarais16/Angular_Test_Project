1357d22e272744a7b155cd3c6577e8ce27b87d32
thamarais16 <thamarais16@gmail.com>
2021-04-09 19:44:18 +0530

    1 file modified, 1 added

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './directives.component.html',
   styleUrls: ['./directives.component.css']
 })
-export class DirectivesComponent implements OnInit {
+export class DirectivesComponent implements OnInit { 
 
   constructor() { }
 
