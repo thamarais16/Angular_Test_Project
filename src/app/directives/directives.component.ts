8c99addb00f5aa715cc3f510f0bb3c3422502bbc
thamarais16 <thamarais16@gmail.com>
2021-04-09 20:02:54 +0530

    2 files modified

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivesComponent implements OnInit { 
 
+   breeds=
+    {
+      "corgi": ["cardigan"],
+      "deerhound": ["scottish"],
+      "bulldog": ["boston", "english", "french"],
+      "mastiff": ["bull", "english", "tibetan"],
+      "australian": ["shepherd"],
+      "greyhound": ["italian"],
+      "buhund": ["norwegian"],
+      "hound": ["afghan", "basset", "blood", "english", "ibizan", "plott", "walker"],
+      "bullterrier": ["staffordshire"],
+    }
+
+    names: string;
+
   constructor() { }
 
   ngOnInit() {
