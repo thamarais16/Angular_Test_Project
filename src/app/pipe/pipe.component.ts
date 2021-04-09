3a97af3306898ee753c22b165d50b2bb2425cb45
thamarais16 <thamarais16@gmail.com>
2021-04-09 18:52:37 +0530

    2 files modified

--- src/app/pipe/pipe.component.ts
+++ src/app/pipe/pipe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, LOCALE_ID, Inject } from '@angular/core';
-import { DatePipe, CurrencyPipe, formatCurrency, getCurrencySymbol, getLocaleDateFormat } from '@angular/common';
+import { DatePipe, CurrencyPipe, formatCurrency, getCurrencySymbol, getLocaleDateFormat, KeyValue } from '@angular/common';
 
 @Component({
   selector: 'app-pipe',
@@ -12,6 +12,26 @@ export class PipeComponent implements OnInit {
   fahrenheit: number;
   celsius: number;
   currency: any;
+  breeds=
+    {
+      "corgi": ["cardigan"],
+      "deerhound": ["scottish"],
+      "bulldog": ["boston", "english", "french"],
+      "mastiff": ["bull", "english", "tibetan"],
+      "australian": ["shepherd"],
+      "greyhound": ["italian"],
+      "buhund": ["norwegian"],
+      "hound": ["afghan", "basset", "blood", "english", "ibizan", "plott", "walker"],
+      "bullterrier": ["staffordshire"],
+    }
+
+    maps = new Map([
+      ['a', 1],
+      ['b', 2],
+      ['c', 3]
+    ])
+
+
   constructor(
     private _date: DatePipe,
     private _currency: CurrencyPipe,
@@ -22,6 +42,12 @@ export class PipeComponent implements OnInit {
     this.toDate = this._date.transform(new Date(8675463526456), 'EE, MMM, d, y, H:m');
     this.currency = formatCurrency(500, this.locale, getCurrencySymbol('INR', 'narrow'));
    // this.toDate = formatDate(new Date(), this.locale, getLocaleDateFormat('EEE','date');
+  }
+
+  orderOriginal = (a: KeyValue<string,string[]>, b: KeyValue<string,string[]>): number => {
+    //return 0;
+    //return a.value.length > b.value.length ? -1 : (a.value.length > b.value.length) ? 0: 1;
+    return a.value.length > b.value.length ? 1 : (a.value.length > b.value.length) ? 0: -1;
   }
 
 }
