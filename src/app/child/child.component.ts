b7e69a9eddf921dbc7f9a4dfc73d2f082d5867a7
thamarais16 <thamarais16@gmail.com>
2021-04-11 18:03:39 +0530

    2 files modified

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -6,38 +6,18 @@ import { Component, OnInit, Input, OnChanges, SimpleChange, SimpleChanges, Outpu
   styleUrls: ['./child.component.css'],
   //inputs: ['count']
 })
-export class ChildComponent implements OnInit, OnChanges {
+export class ChildComponent  {
   
   public _count: number = 0;
-  //@Output() countChange: EventEmitter<number> = new EventEmitter();
 
   constructor() { }
 
-  @Input('MyCount') 
-  set count(value: number){ 
-    console.log(value);
-    this._count = value;
-  }
-
-  get count(): number{
-    return this._count;
-  }
-
-  ngOnInit() {
-  }
-
-  ngOnChanges(changes: SimpleChanges){
-    console.log(changes);
-  }
-
   increment(){
     this._count++;
-    //this.countChange.emit(this._count);
   }
 
   decrement(){
     this._count--;
-    //this.countChange.emit(this._count);
   }
 
 }
