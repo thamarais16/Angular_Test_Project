c7edd799fdcc8c47c14d3727b4fcbc14aaf9c09d
thamarais16 <thamarais16@gmail.com>
2021-04-15 19:53:44 +0530

    5 files modified, 2 added

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styleUrls: ['./child.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class ChildComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  _message;
+  constructor() { 
+    console.log("child constructor");
   }
 
+  customer: Customer = new Customer();
+
+  @Input()
+  set message(value: string){
+    this._message = value;
+  }
+  ngOnInit() {
+    console.log("child init");
+  }
+
+  ngOnDestroy(){
+    console.log("child destroy");
+  }
+
+  ngOnChanges(){
+    console.log("child ngOnChanges");
+  }
+
+  ngDoCheck(){
+    console.log("child ngDoCheck");
+  }
+
+  ngAfterContentInit(){
+    console.log("child ngAfterContentInit");
+  }
+
+  ngAfterContentChecked(){
+    console.log("child ngAfterContentChecked");
+  }
+
+  ngAfterViewInit(){
+    console.log("child ngAfterViewInit");
+  }
+
+  ngAfterViewChecked(){
+    console.log("child ngAfterViewChecked");
+  }
+
+}
+
+
+export class Customer{
+  name: string;
+  code: number;
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-parent',
+  selector: 'app-parent', 
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  styleUrls: ['./parent.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class ParentComponent implements OnInit {
-
-  constructor() { }
+  displayChild: boolean = true;
+  parentMessage: string = "";
+  contentProjection: string = "Hi child . I am parent content projection.";
+  constructor() { 
+    console.log("parent constructor");
+  }
 
   ngOnInit() {
+    console.log("parent init");
+  }
+
+  ngOnDestroy(){
+    console.log("parent destroy");
+  }
+   
+  ngOnChanges(){
+    console.log("parent ngOnChanges");
+  }
+
+  ngDoCheck(){
+    console.log("parent ngDoCheck");
+  }
+
+  ngAfterContentInit(){
+    console.log("parent ngAfterContentInit");
+  }
+
+  ngAfterContentChecked(){
+    console.log("parent ngAfterContentChecked");
+  }
+
+  ngAfterViewInit(){
+    console.log("parent ngAfterViewInit");
+  }
+
+  ngAfterViewChecked(){
+    console.log("parent ngAfterViewChecked");
+  }
+
+  toggle(): void{
+    this.displayChild = !this.displayChild;
   }
 
 }
