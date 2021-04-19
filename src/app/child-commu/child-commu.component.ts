bb5381097087964070fa688c35b9d36d48b7cdcf
thamarais16 <thamarais16@gmail.com>
2021-04-19 15:38:45 +0530

    3 files modified

--- src/app/child-commu/child-commu.component.ts
+++ src/app/child-commu/child-commu.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, DoCheck, KeyValueDiffers, OnChanges } from '@angular/core';
+import { Customer } from '../customer';
 
 @Component({
   selector: 'app-child-commu',
   templateUrl: './child-commu.component.html',
   styleUrls: ['./child-commu.component.css']
 })
-export class ChildCommuComponent implements OnInit {
+export class ChildCommuComponent implements OnInit, DoCheck { 
+  @Input() message: string;
+  @Input() customer = {} as Customer; 
 
   constructor() { }
 
   ngOnInit() { 
+  }
+
+  ngDoCheck(){
   }
 
 }
--- src/app/parent-commu/parent-commu.component.ts
+++ src/app/parent-commu/parent-commu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../customer';
 
 @Component({
   selector: 'app-parent-commu',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent-commu.component.css']
 })
 export class ParentCommuComponent implements OnInit {
-
+  message: string;
+  code: number = 0;
+  name: string = "";
+  customer: Customer = new Customer();
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateChild(){
+    this.customer.name = this.name;
+    this.customer.code = this.code;
   }
  
 }
