2fe7fe31fa074f02f9fe3ccb8fc10b62f760d490
thamarais16 <thamarais16@gmail.com>
2021-04-19 16:48:02 +0530

    1 file modified, 1 added

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from "../product";
+import { ProductService } from "../product-service";
 
 @Component({
   selector: 'app-service',
-  templateUrl: './service.component.html',
+  templateUrl: './service.component.html', 
   styleUrls: ['./service.component.css'] 
 })
-export class ServiceComponent implements OnInit {
+export class ServiceComponent implements OnInit { 
+  products: Product[];
 
   constructor() { }
 
   ngOnInit() {
+    this.products = new ProductService().getProducts();
+    console.log(this.products);
   }
 
 }
