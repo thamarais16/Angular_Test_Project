1d3914969717a10ec7d623894f6637925ad78942
thamarais16 <thamarais16@gmail.com>
2021-06-22 19:08:57 +0530

    1 file modified

--- src/app/cld/cld.component.ts
+++ src/app/cld/cld.component.ts
@@ -11,14 +11,14 @@ export class CldComponent implements OnInit {
   @Input() received = "";
   @Input() customer: Customer<string, number> = new Customer<string, number>();
   changedValue = "";
-  
+
   constructor() { 
     console.log("child constructor");
   }
 
   ngOnChanges(changes: SimpleChanges){
     for(let key in changes){
-      let change = changes['key']
+      let change = changes[key];
       let previousValue =  JSON.stringify(change.previousValue);
       let currentValue =  JSON.stringify(change.currentValue);
       this.changedValue = JSON.stringify(`${previousValue} changed to ${currentValue}`);
